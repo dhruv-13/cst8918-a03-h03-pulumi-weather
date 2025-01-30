e646f0c65393c2a66c5d54cc6706fd0c98ea2340
Rohan Scott <121055475+scot8@users.noreply.github.com>
2025-01-29 22:41:00 -0500

    Update open-weather-service.ts

--- app/api-services/open-weather-service.ts
+++ app/api-services/open-weather-service.ts
@@ -53,19 +53,3 @@ export async function getGeoCoordsForPostalCode(
   const data = response.json()
   return data
 }
-
-export async function fetchWeatherData({
-  lat,
-  lon,
-  units
-}: FetchWeatherDataParams) {
-  const queryString = `lat=${lat}&lon=${lon}&units=${units}`
-
-  const cacheEntry = await redis.get(queryString)
-  if (cacheEntry) return JSON.parse(cacheEntry)
-
-  const response = await fetch(`${BASE_URL}?${queryString}&appid=${API_KEY}`)
-  const data = await response.text() // avoid an unnecessary extra JSON.stringify
-  await redis.set(queryString, data, {PX: TEN_MINUTES}) // The PX option sets the expiry time
-  return JSON.parse(data)
-}
